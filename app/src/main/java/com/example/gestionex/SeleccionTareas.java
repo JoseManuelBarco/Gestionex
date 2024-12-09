6b484f74d717c3ced2a04b1a40b8aad9b6f473a9
JoseManuelBarco <jbarcoc2324@politecnics.barcelona>
2024-12-09 20:21:54 +0100

    Hecho visualizacion i edicion de la tarea

--- app/src/main/java/com/example/gestionex/SeleccionTareas.java
+++
@@ -1,4 +0,0 @@
-package com.example.gestionex;
-
-public class SeleccionTareas {
-}
--- app/src/main/java/com/example/gestionex/Tarea.kt
+++ app/src/main/java/com/example/gestionex/Tarea.kt
@@ -1,9 +1,9 @@
 package com.example.gestionex
 
-class Tarea( val nombreTarea: String,
-             val personaAsignada: String,
-             val estado: String,
-             val fechaInicio: String,
-             val fechaFin: String,
-             val subtareas: List<Subtarea>){
+class Tarea(var nombreTarea: String,
+            var personaAsignada: String,
+            var estado: String,
+            var fechaInicio: String,
+            var fechaFin: String,
+            val subtareas: List<Subtarea>){
 }
