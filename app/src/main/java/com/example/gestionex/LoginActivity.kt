619402cd05d924a109768cccaad5c452b92c3974
JoseManuelBarco <jbarcoc2324@politecnics.barcelona>
2024-12-18 20:25:20 +0100

    Añadido encrypt y decryp del JSON

--- app/src/main/java/com/example/gestionex/LoginActivity.kt
+++ app/src/main/java/com/example/gestionex/LoginActivity.kt
@@ -15,6 +15,11 @@ import com.google.gson.reflect.TypeToken
 import org.mindrot.jbcrypt.BCrypt
 import java.io.File
 import java.io.FileOutputStream
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
 
 class LoginActivity : AppCompatActivity() {
@@ -27,7 +32,7 @@ class LoginActivity : AppCompatActivity() {
         copyJsonUsersToInternalStorage()
         copyJsonProyectsToInternalStorage()
         window.decorView.systemUiVisibility = (
-                        View.SYSTEM_UI_FLAG_FULLSCREEN
+                View.SYSTEM_UI_FLAG_FULLSCREEN
                         or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
 
@@ -36,64 +41,93 @@ class LoginActivity : AppCompatActivity() {
         val passwordEditText = findViewById<EditText>(R.id.PasswordLogin)
         val errorTextView = findViewById<TextView>(R.id.errorTextView)
         val passwordToggle = findViewById<ImageView>(R.id.passwordToggle)
+        val jsonpasswordTextView = findViewById<EditText>(R.id.JSONpasswordLogin)
+        val jsonpasswordToggle = findViewById<ImageView>(R.id.JSONpasswordToggle)
+
 
         var isPasswordVisible = false
+
+        var jsonPasswordisVisible = false
+
         passwordToggle.setOnClickListener {
             isPasswordVisible = !isPasswordVisible
             if (isPasswordVisible) {
-                passwordEditText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_NORMAL
+                passwordEditText.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_NORMAL
                 passwordToggle.setImageResource(R.drawable.showpasswd)
             } else {
-                passwordEditText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                passwordEditText.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
                 passwordToggle.setImageResource(R.drawable.hidepasswd)
             }
 
             passwordEditText.setSelection(passwordEditText.text.length)
         }
-        createUserList()
+        jsonpasswordToggle.setOnClickListener {
+            jsonPasswordisVisible = !jsonPasswordisVisible
+            if (jsonPasswordisVisible) {
+                jsonpasswordTextView.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_NORMAL
+                jsonpasswordToggle.setImageResource(R.drawable.showpasswd)
+            } else {
+                jsonpasswordTextView.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                jsonpasswordToggle.setImageResource(R.drawable.hidepasswd)
+            }
+
+            jsonpasswordTextView.setSelection(jsonpasswordTextView.text.length)
+        }
         loginButton.setOnClickListener {
+            val decryptPassword = jsonpasswordTextView.text.toString()
+            try {
+                createUserList(decryptPassword)
 
-            var i = 0
-            var check = false
+                var i = 0
+                var check = false
 
-            val email = usernameEditText.text.toString().trim()
-            val password = passwordEditText.text.toString().trim()
+                do {
+                    if (userList[i].Correo == usernameEditText.text.toString() && BCrypt.checkpw(
+                            passwordEditText.text.toString(),
+                            userList[i].Contrasenya
 
-            do {
-                if (userList[i].Correo == usernameEditText.text.toString() && BCrypt.checkpw(passwordEditText.text.toString(), userList[i].Contrasenya)) {
-                    check = true
-                    errorTextView.visibility = View.INVISIBLE
-                    val intent = Intent(this, SeleccionProyectos::class.java)
-                    startActivity(intent)
-                    finish()
-                } else {
-                    errorTextView.visibility = View.VISIBLE
-                    errorTextView.text = "CONTRASENYA I/O EMAIL INCORRECTES"
-                }
-                i++
-                if (i == userList.size) {
-                    check = true
-                }
-            } while (!check)
+                        )
+                    ) {
+                        check = true
+                        errorTextView.visibility = View.INVISIBLE
+                        val intent = Intent(this, SeleccionProyectos::class.java)
+                        intent.putExtra("Decrypt_Key",decryptPassword)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        errorTextView.visibility = View.VISIBLE
+                        errorTextView.text = "CONTRASENYA I/O EMAIL INCORRECTES"
+                    }
+                    i++
+                    if (i == userList.size) {
+                        check = true
+                    }
+                } while (!check)
+            } catch (e: Exception) {
+                errorTextView.text = "Formato de los datos incorrecto o contraseña de desbloqueo incorrecta."
+                errorTextView.visibility = View.VISIBLE
+            }
         }
     }
-    private fun createUserList() {
+    private fun createUserList(password: String) {
         val fileName = "usuarios.json"
         val file = File(filesDir, fileName)
 
-        if (file.exists()) {
             try {
-                val jsonText = file.bufferedReader().use { it.readText() }
+                val encryptedText = file.bufferedReader().use { it.readText() }
+
+                val decryptedJson = decryptAES(encryptedText, password)
 
                 val gson = Gson()
                 val userObjects = object : TypeToken<List<User>>() {}.type
 
-                userList = gson.fromJson(jsonText, userObjects)
+                userList = gson.fromJson(decryptedJson, userObjects)
             } catch (e: Exception) {
                 e.printStackTrace()
-            }
-        } else {
-            userList = emptyList()
         }
     }
     private fun copyJsonUsersToInternalStorage() {
@@ -130,6 +164,29 @@ class LoginActivity : AppCompatActivity() {
             } catch (e: Exception) {
                 e.printStackTrace()
             }
+        }
+    }
+
+    @OptIn(ExperimentalEncodingApi::class)
+    private fun decryptAES(encryptedText: String, key: String): String {
+        try {
+            val keyBytes = key.padEnd(32, ' ').substring(0, 32).toByteArray(Charsets.UTF_8)
+            val iv = ByteArray(16)
+
+            val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+
+            val secretKeySpec = SecretKeySpec(keyBytes, "AES")
+            val ivParameterSpec = IvParameterSpec(iv)
+
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec)
+
+            val encryptedBytes = Base64.decode(encryptedText)
+            val decryptedBytes = cipher.doFinal(encryptedBytes)
+
+            return String(decryptedBytes, Charsets.UTF_8)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw Exception("Error al desencriptar datos", e)
         }
     }
 }
--- app/src/main/java/com/example/gestionex/SeleccionProyectos.kt
+++ app/src/main/java/com/example/gestionex/SeleccionProyectos.kt
@@ -8,6 +8,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.File
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
 class SeleccionProyectos : AppCompatActivity() {
     private lateinit var proyectoslist: List<Projectos>
@@ -20,30 +25,56 @@ class SeleccionProyectos : AppCompatActivity() {
         val gridLayoutManager = GridLayoutManager(this, 1)
         recyclerView.layoutManager = gridLayoutManager
 
-        loadJsonFromInternalStorage()
+        val key = intent.getStringExtra("Decrypt_Key")!!
+        createProjectList(key)
 
         val adapter = ProjectoRecyclerView(proyectoslist) { proyecto ->
             val intent = Intent(this, SeleccioTareas::class.java)
             intent.putExtra("PROYECTO_ID", proyecto.proyectoID)
             intent.putExtra("projectName", proyecto.nombreProyecto)
+            intent.putExtra("Decrypt_Key",key)
             startActivity(intent)
         }
 
         recyclerView.adapter = adapter
 
     }
-
-    private fun loadJsonFromInternalStorage() {
+    private fun createProjectList(key: String) {
         val fileName = "proyectos.json"
         val file = File(filesDir, fileName)
+
         try {
-            val jsonText = file.bufferedReader().use { it.readText() }
+            val encryptedText = file.bufferedReader().use { it.readText() }
+            val decryptedJson = decryptAES(encryptedText, key)
+
             val gson = Gson()
-            val projectObjectType = object : TypeToken<List<Projectos>>() {}.type
-            proyectoslist = gson.fromJson(jsonText, projectObjectType)
+            val projectsObjects = object : TypeToken<List<Projectos>>() {}.type
+            proyectoslist = gson.fromJson(decryptedJson, projectsObjects)
+
         } catch (e: Exception) {
             e.printStackTrace()
-            proyectoslist = emptyList()
+        }
+    }
+    @OptIn(ExperimentalEncodingApi::class)
+    private fun decryptAES(encryptedText: String, key: String): String {
+        try {
+            val keyBytes = key.padEnd(32, ' ').substring(0, 32).toByteArray(Charsets.UTF_8)
+            val iv = ByteArray(16)
+
+            val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+
+            val secretKeySpec = SecretKeySpec(keyBytes, "AES")
+            val ivParameterSpec = IvParameterSpec(iv)
+
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec)
+
+            val encryptedBytes = Base64.decode(encryptedText)
+            val decryptedBytes = cipher.doFinal(encryptedBytes)
+
+            return String(decryptedBytes, Charsets.UTF_8)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw Exception("Error al desencriptar datos", e)
         }
     }
 }
